26b02837ab3d9eb80fa8f35652f54cd16a53331f
DiegochaGit <diegochavez287549@correo.itm.edu.co>
2023-03-21 20:46:42 -0500

    Quiz Finished

---
+++ QuizArrays.Logic/OneDimension.cs
@@ -0,0 +1,66 @@
+﻿namespace QuizArrays.Logic
+{
+    public class OneDimension
+    {
+        private int _top; // Logic size
+        private int[] _array;
+
+        public OneDimension(int n)
+        {
+            N = n;
+            _array = new int[n];
+            _top = 0;
+        }
+
+        /// <summary>
+        /// Indicates if the array is empty
+        /// </summary>
+        public bool IsEmpty => _top == 0;
+
+        /// <summary>
+        /// Indicates if the array is full
+        /// </summary>
+        public bool IsFull => _top == N;
+
+        /// <summary>
+        /// Physical size of the array
+        /// </summary>
+        public int N { get; } // Physical size
+
+        public void Add(int number)
+        {
+            if (IsFull)
+            {
+                throw new Exception("The array is full.");
+            }
+
+            _array[_top] = number;
+            _top++;
+        }
+
+        
+
+        public override string ToString()
+        {
+            if (IsEmpty)
+            {
+                return "Empty array.";
+            }
+
+            string output = string.Empty;
+            int count = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                output += $"{_array[i]}\t";
+                count++;
+                if (count > 15)
+                {
+                    count = 0;
+                    output += "\n";
+                }
+            }
+            return output;
+        }
+
+    }
+}
---
+++ QuizArrays.Logic/TwoDimensions.cs
@@ -0,0 +1,83 @@
+﻿namespace QuizArrays.Logic
+{
+    public class TwoDimensions
+    {
+        private int[,] _matrix;
+        private int _m; // rows
+        private int _n; // columns
+
+        public TwoDimensions()
+        {
+            _n = 10;
+            _m = 10;
+            _matrix = new int[_m, _n];
+        }
+
+        public TwoDimensions(int n)
+        {
+            _n = n;
+            _m = n;
+            _matrix = new int[_m, _n];
+        }
+
+        public TwoDimensions(int m, int n)
+        {
+            _n = n;
+            _m = m;
+            _matrix = new int[_m, _n];
+        }
+
+        public int M => _m;
+        public int N => _n;
+
+        public void Fill()
+        {
+            Fill(1, 100);
+        }
+
+        public void Fill(int min, int max)
+        {
+            var random = new Random();
+            for (int i = 0; i < _m; i++)
+            {
+                for (int j = 0; j < _n; j++)
+                {
+                    _matrix[i, j] = random.Next(min, max);
+                }
+            }
+        }
+
+        public OneDimension ToOneDimension()
+        {
+            int sizeVector = M * N;
+            var oneDimension = new OneDimension(sizeVector);
+
+            for (int i = 0; i < _m; i++)
+            {
+                for (int j = 0; j < _n; j++)
+                {
+                    oneDimension.Add(_matrix[i, j]);
+                }
+            }
+
+            return oneDimension;
+        }
+
+
+        public override string ToString()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < _m; i++)
+            {
+                for (int j = 0; j < _n; j++)
+                {
+                    output += $"{_matrix[i, j]}\t";
+                }
+                output += "\n";
+            }
+            return output;
+        }
+
+
+    }
+}
